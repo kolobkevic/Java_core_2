196724a721b616a50cf40f9af5ceeeb81914a5d6
kolobkevic <kolobkevic@google.com>
2022-01-31 17:43:07 +0300

    Java Core 2. Lesson 6.

---
+++ src/main/java/com/kolobkevic/java_core_2/lessons/lesson_6/Main.java
@@ -0,0 +1,8 @@
+package com.kolobkevic.java_core_2.lessons.lesson_6;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new SimpleSingleConsoleClient().start();
+    }
+}
